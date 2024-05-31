bc048ec361ba5398e1d160a5c0ef6d70d5e662a6
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-05-31 16:14:37 +0000

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,10 @@
-
+def print_triangle(triangle):
+  n = len(triangle)
+  if n <= 0:
+    return []
+  triangle = [[1]]
+  for i in range(1,n):
+    triangle.append([1])
+    for j in range(1, i):
+      triangle[i].append(1)
+    return triangle
